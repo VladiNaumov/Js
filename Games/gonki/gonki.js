0c0c5abc5790d50df5dbf1588d8de0715d2cf52a
Simps <vov4.games@yandex.ru>
2022-11-14 16:28:15 +0200

    added

---
+++ Games/gonki/gonki.js
@@ -0,0 +1,203 @@
+// получение элемета canvas который определён в .html
+var canvas = document.getElementById("canvas")
+    // определение контекста рисования (мы рисуем в 2D по этому указываем в скобочках это),
+    //позже данную ссылку ctx мы будем использовать для отрисовки всех объектов
+	var ctx = canvas.getContext("2d")
+	
+	var lives = 5
+	var okLeft = false		
+	var okRight = false	
+	
+    // отрисовки объектов в игре
+	var line = new Image()
+	line.src = "img/line.png"
+	line.X = 180
+	line.Y = -140
+	
+    // отрисовки объектов в игре
+	var line2 = new Image()
+	line2.src = "img/line.png"
+	line2.X = 180
+	line2.Y = 160
+	
+    // отрисовки объектов в игре
+	var myCar = new Image()
+	myCar.src = "img/myCar.png"
+	myCar.X = 50
+	myCar.Y = 400
+
+	// отрисовки объектов в игре
+	var enemyCar1 = new Image()
+	enemyCar1.src = "img/enCar1.png"
+	enemyCar1.X = 50
+	enemyCar1.Y = -150
+	
+    // отрисовки объектов в игре
+	var enemyCar2 = new Image()
+	enemyCar2.src = "img/enCar2.png"
+	enemyCar2.X = 250
+	enemyCar2.Y = -450
+
+	// отрисовка серого прямоугольника
+	function drawRect(){
+		
+		// ctx.fillStyle -задаёт цвет фона
+		ctx.fillStyle = "Gray"
+		
+		// ctx.fillRect -заливаем прямоугольную область
+		ctx.fillRect(0, 0, 400, 500)
+	}
+	
+	// функция для отрисовке в крайне левом углу количество жизней
+	function drawLives(){
+		// ctx.fillStyle -задаёт цвет фона
+		ctx.font = "30px Arial"
+		// ctx.fillStyle -задаёт цвет фона
+		ctx.fillStyle = "White"
+		// ctx.fillText -заливаем текст
+		ctx.fillText("Lives: " + lives, 235, 48)
+	}
+	
+	// функция для отрисовки полосок которые "бегут" в низ
+	function drawLines(){
+		// ctx.drawImage - отрисовываю картинку (плоску) и передаю координаты 
+		ctx.drawImage(line, line.X, line.Y)
+		line.Y +=3
+		
+		if (line.Y > 500){
+			line.Y = -140
+		}
+		
+		ctx.drawImage(line2, line2.X, line2.Y)
+		line2.Y +=3
+		if (line2.Y > 500){
+			line2.Y = -140
+		}
+	}
+	
+	// функция остановки игрового процессса и надпесь GAMEOVER
+	function stop(){
+		
+		// cancelAnimationFrame -фунция для анимаций 
+		cancelAnimationFrame(myReq)
+		ctx.font = "60px Arial"
+		ctx.fillStyle = "Red"
+		ctx.fillText("Game over", 40, 200)
+		stop = true
+	}
+	
+	// функция для отрисовки машины
+	function drawMyCar(){
+		if(okLeft === true && myCar.X > 0) 
+		{
+			myCar.X -=5
+		}
+		if(okRight === true && myCar.X < 335) 
+		{
+			myCar.X +=5
+		}
+		
+		// отрисовываем автомобиль (первый параметр передаем рисунок машины, второй и третий параметр -это координаты машины)
+		ctx.drawImage(myCar, myCar.X, myCar.Y)
+	}
+	
+	// функция для отрисовки машин противника
+	function drawEnemyCar1(){
+	
+		if (enemyCar1.Y+100 > myCar.Y && enemyCar1.X+65 > myCar.X && enemyCar1.X < myCar.X+65){
+			crash = true
+			enemyCar1.Y = enemyCar2.Y - 300
+			lives--
+			if(lives <1){
+				stop()
+			}		
+		}else{
+			crash = false
+		}	
+	
+		if (!crash){
+			// отрисовка машины
+			ctx.drawImage(enemyCar1, enemyCar1.X, enemyCar1.Y)
+			enemyCar1.Y +=2
+			if (enemyCar1.Y > 500){
+				enemyCar1.Y = -100
+				enemyCar1.X = Math.floor(Math.random()*335)
+			}
+		}
+	}
+	
+	// функция для отрисовки машин противника
+	function drawEnemyCar2(){
+	
+		if (enemyCar2.Y+100 > myCar.Y && enemyCar2.X+65 > myCar.X && enemyCar2.X < myCar.X+65){
+			crash = true
+			enemyCar2.Y = enemyCar1.Y - 300
+			lives--
+			if(lives <1){
+				stop()
+			}		
+		}else{
+			crash = false
+		}	
+	
+		if (!crash){
+			// отрисовка машины
+			ctx.drawImage(enemyCar2, enemyCar2.X, enemyCar2.Y)
+			enemyCar2.Y +=2
+			if (enemyCar2.Y > 500){
+				enemyCar2.Y = -100
+				enemyCar2.X = Math.floor(Math.random()*335)
+			}
+		}
+	}   
+	
+    // вызов функцийб т.е игровой процесс 
+	function render(){
+		
+		// если переменная STOP равна true то игра завершается
+		if (stop === true)
+		{
+			return
+		}
+	
+		drawRect()
+		drawLives()
+		drawLines()
+		drawMyCar()
+		drawEnemyCar1()
+		drawEnemyCar2()		
+		
+        // requestAnimationFrame() -специальная функция для отрисовки анимаций
+		myReq = requestAnimationFrame(render)
+	}
+
+    // начало игрового процесса
+	render()	
+	
+	
+	/* ОБРАБОТЧИКИ СОБЫТИЙ НАЖАТИЯ КЛАВИШ */
+	
+	// функция addEventListener -обработчик нажатия клавишу
+	addEventListener("keydown", function(event){
+		var newDirect = event.keyCode
+		if (newDirect === 37){
+			okLeft = true
+		}
+		
+		if (newDirect === 39){
+			okRight = true
+		}
+	})
+	
+	// функция addEventListener -обработчик нажатия клавишу
+	addEventListener("keyup", function(event){
+		var newDirect = event.keyCode
+		if (newDirect === 37){
+			okLeft = false
+		}
+		
+		if (newDirect === 39){
+			okRight = false
+		}
+	})
+
---
+++ Games/pingpong/pingpong.js
@@ -0,0 +1,156 @@
+// получение элемета canvas который определён в .html
+var canvas = document.getElementById("canvas")
+    // определение контекста рисования (мы рисуем в 2D по этому указываем в скобочках это),
+    //позже данную ссылку ctx мы будем использовать для отрисовки всех объектов
+	var ctx = canvas.getContext("2d")
+	
+	// переменные для отслеживания нажатие клавиши влево или вправо
+	var okLeft = false		
+	var okRight = false	
+	
+	// отрисовка фона
+	var drawRect = function(){
+		// установка цвета области заливки
+		ctx.fillStyle = "khaki"
+		// установка области заливки 
+		ctx.fillRect(0, 0, 400, 500)		
+	}
+	
+	// функция для отрисовки мячика
+	var circle = function(x, y){
+		// ctx.fillStyle -задаем цвет
+		ctx.fillStyle = "brown"
+		// ctx.beginPath() -отрисовки дуги
+		ctx.beginPath()
+		// передаём координаты окружностиб радиус окружности в радианах
+		ctx.arc(x, y, 10, 0, 6.28, false)
+		// ctx.fill() -выполняющий заливку
+		ctx.fill()
+	}
+	
+	// функция для остановки анимации
+	var stop = function(){
+		// функция вызова анимации
+		cancelAnimationFrame(myReq)
+		
+		// рисуем слово GAME OVER
+		ctx.font = "60px Arial"
+		ctx.fillStyle = "red"
+		ctx.fillText("Game over", 40, 200)
+		stop = true
+	}
+	
+	// платформа
+	class Platform{
+		constructor(x, y){
+			this.x = x
+			this.y = y
+		}
+		
+		// это метод отрисовывает платформу в игре
+		drawPlatform(){
+			if(okLeft === true && this.x > 0) 
+			{
+				this.x -=5
+			}
+			
+			if(okRight === true && this.x < 300) 
+			{
+				this.x +=5
+			}
+			
+			// заливка цветом
+			ctx.fillStyle = "teal"
+			// координаты платформы и её размер
+			ctx.fillRect(this.x, this.y, 100, 20)
+		}
+	}
+
+			
+	// мяч
+	class Ball {
+		constructor(x, y){
+			this.x = x
+			this.y = y
+		}
+		
+		//
+		addX = 2
+		//
+		addY = -3
+		
+		// метод отрисовки шарика
+		drawBall(){
+			circle(this.x, this.y)
+			this.x +=this.addX
+			this.y +=this.addY
+			
+			if (this.x + 10 > 400){				
+				this.addX = - this.addX				
+			}
+			
+			if (this.x - 10 < 0){
+				this.addX = -this.addX
+			}
+			
+			if (this.y - 10 < 0){				
+				this.addY = - this.addY				
+			}
+			
+			if (this.y + 10 === 480 && this.x + 10 > myPlatf.x && this.x - 10 < myPlatf.x + 100){
+				this.addY = - this.addY
+			}	
+			
+			if (this.y - 10 > 500){
+				stop()
+			}
+		}
+	}
+	
+	// создаём экземпляр класса Platform
+	var myPlatf = new Platform(150, 480)
+	// создаём экземпляр класса Ball	
+	var myBall = new Ball(200, 470)		
+	
+	// основная функция для запуска игры
+	var render = function(){	
+		if (stop === true)
+		{
+			return
+		}
+		
+		drawRect()
+		myPlatf.drawPlatform()
+		myBall.drawBall()
+		
+		// requestAnimationFrame() -специальная функция для отрисовки анимаций
+		myReq = requestAnimationFrame(render)
+	}
+	
+	// функция запуска игры
+	render()
+	
+	
+	/* УПРАВЛЕНИЕ КЛАВИАТУРОЙ */
+	
+	addEventListener("keydown", function(event){
+		var newDirect = event.keyCode
+		if (newDirect === 37){
+			okLeft = true
+		}
+		
+		if (newDirect === 39){
+			okRight = true
+		}
+	})
+	
+	addEventListener("keyup", function(event){
+		var newDirect = event.keyCode
+		if (newDirect === 37){
+			okLeft = false
+		}
+		
+		if (newDirect === 39){
+			okRight = false
+		}
+	})
