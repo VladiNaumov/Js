f19f7a762aa6c1389ca502155c147dae5e47e6f6
Simps <vov4.games@yandex.ru>
2022-11-20 15:56:14 +0200

    correction

--- Games/_15_pazle/app.js
+++ Games/_15_pazle/app.js
@@ -1,6 +1,5 @@
 'use strict'
 const field = document.querySelector('.field');
-
 const cellSize = 100;
 
 const empty = {
@@ -8,6 +7,33 @@ const empty = {
     left: 0
 };
 
+const cells = [];
+cells.push(empty);
+
+// 
+function move(index){
+   const cell = cells[index];
+
+    const leftDiff = Math.abs (empty.left - cell.left);
+    const topDiff = Math.abs (empty.top - cell.top);
+
+    if(leftDiff + topDiff > 1){
+        return;
+    }
+
+    
+    cell.element.style.left = `${empty.left * cellSize}px`;
+    cell.element.style.top =  `${empty.top * cellSize}px`;
+
+    const emptyLeft = empty.left;
+    const emptyTop = empty.top;
+
+    empty.left = cell.left;
+    empty.top = cell.top;
+
+    cell.left = emptyLeft;
+    cell.top = emptyTop;
+}
 
 for(let i = 1; i <= 15; i++){
     const cell = document.createElement('div');
@@ -19,16 +45,21 @@ for(let i = 1; i <= 15; i++){
     // позиция ячейки вниз
     const top = (i - left) / 4;
 
+    cells.push({
+        left: left,
+        top: top,
+        element: cell
+    });
     
+
     cell.style.left = `${left * cellSize}px`;
     cell.style.top =  `${top * cellSize}px`;
 
-   
-
     field.append(cell);
 
-   // field.innerHTML = cell;
-    // console.log(left);
-
+ 
+    cell.addEventListener('click', () => {
+        move(i);
+    });
 }
-
+   
