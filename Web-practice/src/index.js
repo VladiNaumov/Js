276abe9547a286b1077137825dfd039cf6c64fc8
Simps <vov4.games@yandex.ru>
2022-12-05 20:55:20 +0200

    correction

--- Web-practice/src/index.js
+++ Web-practice/src/index.js
@@ -3,47 +3,98 @@
 
 /* FILE modul.js  */
 const model = [
-    { type: 'title', value: 'Hello World from JS'},
-    { type: 'text', value: 'here we go with some text'},
-    { type: 'columns', value: [
-        '1111111',
-        '2222222',
-        '3333333'
-    ]}
+    { 
+        type: 'title', 
+        value: 'конструктор сайтов на чистом JS', 
+        options: {
+            tag: 'h2',
+            styles: {
+                backgraund: 'linear-gradient(to right, #ff0099, #493240',
+                color: '#fff',
+                padding: '1.5rem',
+                'text-aling': 'center'
+
+            } 
+
+    }},
+
+    { 
+        type: 'columns', 
+        value: [
+            'Приложение на чистом JavaScript, без использования библиотек ',
+            'JavaScript - это просто, интересно. Научись создавать любые UI своими руками. ',
+            'badabadabda',
+            'habababababa'
+        ], options: {
+                styles: {
+                backgraund: 'linear-gradient(to right, #ff0099, #493240',
+                padding: '2rem',
+                color: '#fff',
+                'text-weiting': 'bold'
+
+            } }
+        
+    },
+
+    {
+        type: 'text', 
+        value: [
+            'Крутые видео и уроки по JavaScript тут: ', 
+            'Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам,',
+            'такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!'
+
+        ], options: {
+            styles: {
+                background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+                padding: '1rem',
+                'font-weight': 'bold'
+
+            } }
+    }
     
 ]
 
 /* END */
 
+
+
 /* utils.js */
 
-function row(content){
-    return `<div class="row">${content}</div>`
+function row(content, styles = ''){
+    return `<div class="row" styles ="${styles}">${content}</div>`
 }
 
 function col(content){
     return `<div class="col-sm">${content}</div>`
 }
 
+function css(styles = {}){
+    const toString = key => `${key}: ${styles[key]}`
+    return Object.keys(styles).map(toString).join(';')
+}
+
 /* END */
+
+
 
 /* FILE template.js*/
 
 function title (block){
-  
-    return row(col(`<h1>${block.value}</h1>`))
+    const {tag = 'h1', styles} = block.options
+    //console.log(styles)
+    return row(col(`<${tag}>${block.value}</${tag}>`), css(styles))
 }
 
 function text (block){
-  
-    return row(col(`<p>${block.value}</p>`))
+    return row(col(`<p>${block.value}</p>`), css(block.options.styles))
 }
 
 
 
 function columns(block){
     const html = block.value.map(item => col(item)).join('')
-    return row(html)
+   // console.log(html)
+    return row(html,css(block.options.styles))
 }
 
 const templates ={
