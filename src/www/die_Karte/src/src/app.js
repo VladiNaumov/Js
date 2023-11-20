558fee5cd8cda856f9d51dbea760321ec30f6e1a
NaumDeveloper <vov4.games@yandex.ru>
2023-11-20 19:07:12 +0200

    correction

--- src/www/die_Karte/src/src/app.js
+++ src/www/die_Karte/src/src/app.js
@@ -1,4 +1,14 @@
-import {nodes} from "./node.js";
+const nodes = [
+    { isIst: true, deu: 'der Hunger', rus: 'голод', isTranslate: false },
+
+    { isIst: true, deu: 'die Costen', rus: 'покупать', isTranslate: false },
+
+    { isIst: true, deu: 'jetzt', rus: 'сейчас', isTranslate: false },
+
+    { isIst: true, deu: 'spät', rus: 'поздно', isTranslate: false },
+
+    { isIst: true, deu: 'ainkaufen', rus: 'покупать', isTranslate: false },
+]
 
 const inputElementRus = document.getElementById('deu')
 const inputElementDeu = document.getElementById('rus')
