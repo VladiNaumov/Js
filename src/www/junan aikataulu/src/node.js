ba9795d02c00d367a3984d36a0f6dbc0abb88167
NaumDeveloper <vov4.games@yandex.ru>
2023-11-21 22:09:56 +0200

    correction

---
+++ src/www/junan aikataulu/src/node.js
@@ -0,0 +1,11 @@
+export const nodes = [
+    { isIst: true, matkaa: 'der Hunger', aika: '05.00'},
+
+    { isIst: true, matkaa: 'die Costen', aika: '21.00' },
+
+    { isIst: true, matkaa: 'jetzt', aika: '13.05' },
+
+    { isIst: true, matkaa: 'spät', aika: '17.20'},
+
+    { isIst: true, matkaa: 'ainkaufen', aika: '22.00' },
+]
